ac28553119e63ce2b1129aee4a318b79955bedd7
Kaushal Singh <kaushalsingh8178@gmail.com>
2023-12-29 04:25:42 +0530

    first commit

---
+++ app .js
@@ -0,0 +1,26 @@
+
+
+
+const parent= React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"hlo h1 tagg"),
+        React.createElement("h2",{},"hlo h2 tagg"),
+    
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"hlo h1 tagg"),
+        React.createElement("h2",{},"hlo h2 tagg"),
+    
+    ])
+])
+
+
+
+
+
+
+
+
+const hading= React.createElement("h1",{id:"hading"},"hlow word")
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(parent)
